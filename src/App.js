4aec510bf6036bdb8746c16ac39f2a60632d2904
saurabh mishra <saurabh.mishra@accubits.com>
2023-01-25 22:49:56 +0530

    front-end ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login";
 
-import './App.css';
-import Footer from './components/Footer/Footer';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
-import Home from "./components/Home/Home"
-import Header from "./components/Header/Header"
-import About from "./components/About/About"
-import Projects from "./components/Projects/Projects"
 function App() {
-  return <Router>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <Route path="/About" element={<About/>}/>
-      <Route path="/Projects" element={<Projects/>}/>
-
-    </Routes>
-    <Footer/>
-  </Router>
+        <Route path="/About" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/account" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,53 +1,68 @@
-import React from 'react'
-import { Typography , Button } from '@mui/material'
-import {AiOutlineProject} from "react-icons/ai"
-import {Delete} from "@mui/icons-material"
-import "./Projects.css"
+import React from "react";
+import { Typography, Button } from "@mui/material";
+import { AiOutlineProject } from "react-icons/ai";
+import { Delete } from "@mui/icons-material";
+import "./Projects.css";
+import { FaRegSmileWink } from "react-icons/fa";
 
-const ProjectCard=({
-    url,
-    projectTitle,
-    projectImage,
-    description,
-    technologies,
-    isAdmin=false
-})=>{
-    return(
-        <>
-        <a href={url} className="projectCard" target="blank">
-            <div>
-                <img src={projectImage} alt="Projectimage"/>
-<Typography variant="h5" className="projectTitle">{projectTitle}</Typography>
-                </div>
-                <div>
-                    <Typography variant="h4">About Project</Typography>
-                    <Typography>{description}</Typography>
-                    <Typography variant="h6"> Tech Stack:{technologies}</Typography>
-                </div>
-        </a>
-        {
-            isAdmin &&(<Button style={{ color:"rgba(40,40,40,0.7)"}}> <Delete/></Button>)
-        }
-        </>
-    )
-}
-const Projects = () => {
-    const projects=[1,2,3,4,5]
+const ProjectCard = ({
+  url,
+  projectTitle,
+  projectImage,
+  description,
+  technologies,
+  isAdmin = false,
+}) => {
   return (
-    <div className='projects'>
-      <Typography variant="h3"> Projects <AiOutlineProject/></Typography>
-      <div className='projectsWrapper'>
-        {projects.map((projects,index)=>(
-            <ProjectCard className="projectCard"  url="https://github.com/saurabh1352/portfolio-1.git"
+    <>
+      <a href={url} className="projectCard" target="blank">
+        <div>
+          <img src={projectImage} alt="Projectimage" />
+          <Typography variant="h5" className="projectTitle">
+            {projectTitle}
+          </Typography>
+        </div>
+        <div>
+          <Typography variant="h4">About Project</Typography>
+          <Typography>{description}</Typography>
+          <Typography variant="h6"> Tech Stack:{technologies}</Typography>
+        </div>
+      </a>
+      {isAdmin && (
+        <Button style={{ color: "rgba(40,40,40,0.7)" }}>
+          {" "}
+          <Delete />
+        </Button>
+      )}
+    </>
+  );
+};
+const Projects = () => {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <div className="projects">
+      <Typography variant="h3">
+        {" "}
+        Projects <AiOutlineProject />
+      </Typography>
+      <div className="projectsWrapper">
+        {projects.map((projects, index) => (
+          <ProjectCard
+            className="projectCard"
+            url="https://github.com/saurabh1352/portfolio-1.git"
             projectTitle=" Cricket multipplayer"
             projectImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAC_WueCSCX7Nk4XA5Zo7S3jd7Wy-HPkBJvyWik3zrcnnp2xmLybp6MQx9WvoaKGpyE0&usqp=CAU"
             description=""
-            technologies="node js,express js, colysius npm dependency for room creation,mongodb"  />
-            
+            technologies="node js,express js, colysius npm dependency for room creation,mongodb"
+          />
         ))}
       </div>
+      <Typography variant="h3" style={{ font: "100 1.2rem Ubunto Mono " }}>
+        All The Projects Shown Above are Made by Me
+        <FaRegSmileWink />
+      </Typography>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
