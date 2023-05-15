95abc32450db3878f2d7ab9047d5f6a95511838b
saurabh1352 <mishrasau91@gmail.com>
2023-05-15 06:45:09 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Login from "./components/Login/Login";
 
+
 function App() {
   return (
     <Router>
---
+++ src/Store.js
@@ -0,0 +1,5 @@
+import {configureStore} from "@redux.js/toolkit";
+const store=configureStore({
+    reducer:{},
+})
+export default store;
